e1a916f6d1c83d84916a34cc25924bafd12bcbe9
Iliyan Bogdanov <iliyanbogdanov@gmail.com>
2024-05-13 15:57:51 +0300

    Unsuccessful login - users[1]

--- tests/loginPageElements.ts
+++ tests/loginPageElements.ts
@@ -1,3 +1,5 @@
 export const USERNAME_FIELD_SELECTOR = '#user-name';
 export const PASSWORD_FIELD_SELECTOR = '#password';
 export const LOGIN_BUTTON_SELECTOR = '#login-button';
+export const INVENTORY_LIST = '.inventory_list';
+export const VALIDATION_MESSAGE_ERROR = '.error-message-container';
--- tests/loginPageTests.spec.ts
+++ tests/loginPageTests.spec.ts
@@ -1,36 +1,42 @@
 // test.spec.ts
 import { expect, test } from '@playwright/test';
 import * as userCredentials from '../user-credentials.json';
-import { USERNAME_FIELD_SELECTOR, PASSWORD_FIELD_SELECTOR, LOGIN_BUTTON_SELECTOR } from './loginPageElements';
+import { USERNAME_FIELD_SELECTOR, PASSWORD_FIELD_SELECTOR, LOGIN_BUTTON_SELECTOR, INVENTORY_LIST, VALIDATION_MESSAGE_ERROR } from './loginPageElements';
 
-// Define test suite
 test.describe('Swag Labs QA Automation Test Suite', () => {
-    
-    // Define test case
-    test('Successful login', async ({ page }) => {
 
-        // Navigate to Swag Labs homepage
+    test('Successful login - users[0]', async ({ page }) => {
+
         await page.goto('https://qa-challenge.codesubmit.io/');
 
-        // Access the first user object from the users array
         const firstUser = userCredentials.users[0];
-
-        // Extract username and password from the first user object
         const { username, password } = firstUser;
 
-        // Fill username and password fields
         await page.fill(USERNAME_FIELD_SELECTOR, username);
         await page.fill(PASSWORD_FIELD_SELECTOR, password);
-
-        // Click on the login button
         await page.click(LOGIN_BUTTON_SELECTOR);
 
-        // Wait for the dashboard to load
-        await page.waitForSelector('.inventory_list');
+        await page.waitForSelector(INVENTORY_LIST);
 
-        // Assert that the inventory list is visible
-        const inventoryList = await page.isVisible('.inventory_list');
+        const inventoryList = await page.isVisible(INVENTORY_LIST);
         expect(inventoryList).toBeTruthy();
 
+    });
+
+    test('Unsuccessful login - users[1]', async ({ page }) => {
+
+        await page.goto('https://qa-challenge.codesubmit.io/');
+
+        const firstUser = userCredentials.users[1];
+        const { username, password } = firstUser;
+
+        await page.fill(USERNAME_FIELD_SELECTOR, username);
+        await page.fill(PASSWORD_FIELD_SELECTOR, password);
+        await page.click(LOGIN_BUTTON_SELECTOR);
+
+        const actualValidationMessage = await page.innerText(VALIDATION_MESSAGE_ERROR);
+        const expectedValidationMessage = 'Epic sadface: Sorry, this user has been locked out.';
+
+        expect(actualValidationMessage).toBe(expectedValidationMessage);
     });
 });
